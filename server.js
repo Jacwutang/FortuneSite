050b9da476744470bd1f2309ef968aad2fdfe1ac
jack <jack@jacks-air.lan>
2024-03-20 18:51:45 -0700

    change port to 80

--- server.js
+++ server.js
@@ -164,6 +164,6 @@ app.post('/get', function (req, res) {
   
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(80, function () {
+  console.log('Example app listening on port 80!')
 })
